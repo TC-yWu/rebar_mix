2acc5fe1bbca6c9d840a98132c0aa9d40faef7f0
Y.P Wu <ywu@tigerconnect.com>
2025-05-11 17:17:34 +0800

    [USR-156]

--- src/rebar_mix_compiler.erl
+++ src/rebar_mix_compiler.erl
@@ -18,19 +18,21 @@ build(AppInfo) ->
 
   rebar_mix_utils:compile(AppDir),
 
-  {ok, Apps} = rebar_utils:list_dir(BuildElixirDir),
-  Deps = Apps -- [AppName],
-  rebar_mix_utils:move_to_path(Deps, BuildElixirDir, BuildDir),
-
-  AppBuild = filename:join(AppDir, "_build/prod/lib/" ++ AppName ++ "/ebin"),
-  AppTaget = filename:join(AppDir, "ebin"),
-  ec_file:copy(AppBuild, AppTaget, [recursive]),
-
-  Lock = rebar_mix_utils:create_rebar_lock_from_mix(AppDir, Deps),
-  ElixirLock = rebar_mix_utils:elixir_to_lock(Lock),
-  rebar_mix_utils:save_rebar_lock(AppDir, ElixirLock),
-  rebar_mix_utils:delete(filename:join(AppDir, "_build")),
-
+  case rebar_utils:list_dir(BuildElixirDir) of
+    {ok, Apps} ->
+      Deps = Apps -- [AppName],
+      rebar_mix_utils:move_to_path(Deps, BuildElixirDir, BuildDir),
+      AppBuild = filename:join(AppDir, "_build/prod/lib/" ++ AppName ++ "/ebin"),
+      AppTaget = filename:join(AppDir, "ebin"),
+      ec_file:copy(AppBuild, AppTaget, [recursive]),
+    
+      Lock = rebar_mix_utils:create_rebar_lock_from_mix(AppDir, Deps),
+      ElixirLock = rebar_mix_utils:elixir_to_lock(Lock),
+      rebar_mix_utils:save_rebar_lock(AppDir, ElixirLock),
+      rebar_mix_utils:delete(filename:join(AppDir, "_build"));
+    _ ->
+      skip
+  end,
   ok.
 
 format_error({mix_not_found, Name}) ->
